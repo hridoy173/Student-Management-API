60af7d449b699c3a4c5f8a8625769d9abf205a0a
MAS HRIDOY <shekhlaravel@gmail.com_for_shekh173>
2026-07-20 20:39:40 +0600

    aeasye

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const errorHandler = require("./middlewares/errorHandler");
+
 const routes = require("./routes");
 
-
+// Middleware to parse JSON request bodies
 app.use(express.json());
 
 
+// Use the routes
 app.use("/api", routes);
 
 
+
+// Error handling middleware
+app.use(errorHandler);
 
 
 module.exports = app;
